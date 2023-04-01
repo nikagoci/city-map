5ab426f94d3e1ddb07b9135b73bd0fadddf20bb4
nikagoci <dato.svanidze2005@mail.ru>
2023-04-01 19:17:27 +0400

    added information panel for user to get information

---
+++ components/information/info-button.tsx
@@ -0,0 +1,26 @@
+import {useState } from 'react'
+
+import Modal from "./modal";
+
+export default function InfoButton() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function closeModal() {
+        setIsOpen(false)
+      }
+    
+      function openModal() {
+        setIsOpen(true)
+      }
+
+  return (
+    <>
+    <Modal isOpen={isOpen} openModal={openModal} closeModal={closeModal} />
+    <button onClick={openModal} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        Map Guide Helper
+      </span>
+    </button>
+    </>
+  );
+}
---
+++ components/information/modal.tsx
@@ -0,0 +1,97 @@
+import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { Fragment } from "react";
+import { MdMyLocation } from "react-icons/md";
+
+const markerWithColors = [
+  {category: 'Aquatica', src: '/markers/blue-marker.svg'},
+  {category: 'Reserve', src: '/markers/green-marker.svg'},
+  {category: 'Bridge', src: '/markers/orange-marker.svg'},
+  {category: 'Garden', src: '/markers/lime-marker.svg'},
+  {category: 'Monastery', src: '/markers/red-marker.svg'},
+  {category: 'Museum', src: '/markers/dark-marker.svg'},
+]
+
+interface Props {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+export default function Modal({ isOpen, openModal, closeModal }: Props) {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-full p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Map Guide Helper
+                  </Dialog.Title>
+                  <div className="mt-6">
+                    {/* <p className="text-sm text-gray-500">
+                      Your payment has been successfully submitted. We’ve sent
+                      you an email with all of the details of your order.
+                    </p> */}
+                    {markerWithColors.map(marker => (
+                      <div className="flex items-center mb-3">
+                        <Image src={marker.src} alt={marker.category} width={28} height={28} />
+                        <span className="w-12 h-[3px] mx-5 bg-cyan-600"></span>
+                        <h3 className="text-lg font-semibold">{marker.category}</h3>
+                      </div>
+                    ))}
+                    <div className="flex items-center">
+                      <MdMyLocation size={40} color="blue" className='current-location' />
+                      <span className="w-12 h-[3px] mx-5 bg-cyan-600"></span>
+                      <h3 className="text-lg font-semibold">Current Location</h3>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center mt-8">
+                    <span className="mr-2 text-lg font-bold">NOTICE:</span>
+                    <h3 className="underline ">Click any marker on map to hear information.</h3>
+                  </div>
+
+                  <div className="mt-8">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
